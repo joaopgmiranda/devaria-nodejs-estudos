8457d672cfe40a3819e61cf1157fd9a5b1894140
joaopgmiranda <joaomiranda.ufjf@gmail.com>
2022-11-18 14:09:08 -0300

    variáveis e input

---
+++ variaveis-e-input/app.js
@@ -0,0 +1,17 @@
+const readLine = require('readline').createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+const minhaPrimeiraConstanteString = 'Minha primeira constante';
+console.log(minhaPrimeiraConstanteString);
+
+let leituraDeCampo;
+let teste = 'teste'
+readLine.question('Informe sua idade:', t => {
+    leituraDeCampo = t;
+    console.log(`o usuario digitou: ${leituraDeCampo} e nossa variavel de teste e: ${teste}`);
+});
+
+
+
